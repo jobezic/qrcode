40f7f5db50f54b0a6c7335861b22e8e0a56d9511
Giovanni Bezicheri <giovanni.bezicheri@rindus.de>
2022-01-02 16:31:02 +0000

    Fix style for admin menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,23 @@
 import Button from '@mui/material/Button'
 import MenuList from './MenuList'
 import Container from '@mui/material/Container'
+import Box from "@mui/material/Box";
 
 const Menu = (props) => (
-  <Container sx={{m: "1em"}}>
-    <h1>Menu</h1>
-    {props.showAddMenuItem && <div className="panel">
-      <Button variant="contained" onClick={props.showAddMenuItem}>Add Item</Button>
-    </div>}
-    <MenuList foods={props.dbData} removeMenuItem={props.removeMenuItem} />
-  </Container>
+  <Box
+  display="flex"
+  justifyContent="center"
+  alignItems="center"
+  minHeight="100vh"
+>
+    <Container sx={{m: "1em"}} maxWidth="sm">
+      <h1>Menu</h1>
+      {props.showAddMenuItem && <div className="panel">
+        <Button variant="contained" onClick={props.showAddMenuItem}>Add Item</Button>
+      </div>}
+      <MenuList foods={props.dbData} removeMenuItem={props.removeMenuItem} />
+    </Container>
+  </Box>
 )
 
 export default Menu
