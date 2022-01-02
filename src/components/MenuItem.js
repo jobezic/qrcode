a9e7b7d034fb62e1c998519dc1538ad62d5d5ada
Giovanni Bezicheri <giovanni.bezicheri@rindus.de>
2022-01-02 16:12:51 +0000

    Fix style for readers menu

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -3,9 +3,9 @@ import Button from '@mui/material/Button'
 const MenuItem = (props) => (
   <div className="card">
     <div className="image">
-      {props.dish.img ? <img src={props.dish.img} alt="" width="512" /> : <img src="/food-tray.png" alt="" width="32" />}
+      {props.dish.img ? <img src={props.dish.img} alt="" /> : <img src="/food-tray.png" alt="" width="32" />}
     </div>
-    <span>{props.dish.title}</span>
+    <h3>{props.dish.title}</h3>
     <p>{props.dish.desc}</p>
     {props.removeMenuItem && <Button variant="outlined" onClick={props.removeMenuItem}>Remove</Button>}
   </div>
