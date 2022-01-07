92a1736027e46403e8079f87b58d2f317676380c
Giovanni Bezicheri <giovanni.bezicheri@rindus.de>
2022-01-07 16:04:05 +0000

    Add loading text when fetching menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,21 +3,26 @@ import MenuList from './MenuList'
 import Container from '@mui/material/Container'
 import Box from "@mui/material/Box";
 
-const Menu = (props) => (
-  <Box
-  display="flex"
-  justifyContent="center"
-  alignItems="center"
-  minHeight="100vh"
->
-    <Container sx={{m: "1em"}} maxWidth="sm">
-      <h1>Menu</h1>
-      {props.showAddMenuItem && <div className="panel">
-        <Button variant="contained" onClick={props.showAddMenuItem}>Add Item</Button>
-      </div>}
-      <MenuList foods={props.dbData} removeMenuItem={props.removeMenuItem} />
-    </Container>
-  </Box>
-)
+const Menu = (props) => {
+  if (props.dbData.length === 0) {
+    return <h3>Loading...</h3>
+  }
+
+  return (
+    <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    minHeight="100vh"
+  >
+      <Container sx={{m: "1em"}} maxWidth="sm">
+        <h1>Menu</h1>
+        {props.showAddMenuItem && <div className="panel">
+          <Button variant="contained" onClick={props.showAddMenuItem}>Add Item</Button>
+        </div>}
+        <MenuList foods={props.dbData} removeMenuItem={props.removeMenuItem} />
+      </Container>
+    </Box>)
+}
 
 export default Menu
